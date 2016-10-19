5eb18f630a3a13be92082b721722bd9fc2ea309f
BobB <bob.buehler@gmail.com>
2016-10-19 20:27:46 +0000

    Changed demo puzzle in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -6,65 +6,35 @@ const F = false;
 // http://venturebeat.com/community/2009/08/24/picross-does-what-sudontku/
 const puzzle = new Picross.Puzzle();
 puzzle.rowRules = [
-    [4, 4],
-    [6, 6],
-    [6, 6],
-    [6, 6],
-    [6, 6],
-    [4, 4],
-    [10, 10],
-    [12, 12],
-    [12, 12],
-    [12, 12],
-    [12, 12],
-    [12, 12],
-    [12, 12],
-    [1, 8, 1, 1, 8, 1],
-    [3, 3, 3, 3],
-    [3, 3, 3, 3],
-    [3, 3, 3, 3],
-    [3, 3, 3, 3],
-    [3, 3, 3, 3],
-    [3, 3, 3, 3]
+    [2],
+    [4],
+    [2, 2],
+    [2, 2],
+    [2, 2, 2],
+    [1, 2, 1],
+    [1, 2, 1],
+    [1, 2, 2, 1],
+    [1, 2, 2, 1],
+    [1, 2, 1]
 ];
 puzzle.colRules = [
-    [0],
-    [7],
-    [7],
-    [14],
-    [4, 14],
-    [20],
-    [14],
-    [14],
-    [20],
-    [4, 14],
-    [14],
-    [7],
-    [7],
-    [0],
-    [0],
-    [0],
-    [0],
-    [7],
-    [7],
-    [14],
-    [4, 14],
-    [20],
-    [14],
-    [14],
-    [20],
-    [4, 14],
-    [14],
-    [7],
-    [7],
-    [0]
+    [6],
+    [2],
+    [2, 2],
+    [2, 2, 2],
+    [2, 2],
+    [2],
+    [2, 4],
+    [2, 4],
+    [2],
+    [6]
 ];
 
 const solver = new Picross.Solver(puzzle);
 while(true) {
     const line: Picross.Line = solver.step();
     if (line) {
-        console.log(line);
+        console.log(`${line.isRow ? 'r' : 'c'}-${line.index}`);
         console.log(solver.getPartial().toString());
     } else {
         break;
